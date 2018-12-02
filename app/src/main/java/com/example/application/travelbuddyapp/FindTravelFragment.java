9c297e4877d7774e77b62194a55ffea3fbf1016a
naman16169 <naman16169@iiitd.ac.in>
2018-12-02 14:36:05 -0800

    Add Trip imlemented

--- app/src/main/java/com/example/application/travelbuddyapp/FindTravelFragment.java
+++ app/src/main/java/com/example/application/travelbuddyapp/FindTravelFragment.java
@@ -18,13 +18,14 @@ import android.widget.RadioGroup;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FindTravelFragment extends Fragment {
+public class FindTravelFragment extends Fragment implements TravelDialog.TravelDialogListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     TravelAdapter adapter;
     RecyclerView recyclerView;
-    List<travel> travelList;
-    View view;
+    List<travel> travelList= new ArrayList<>();
+    static View view;
+    String rtripDetail, rPlace, rDate, rCity, rtripName;
 
     public FindTravelFragment() {
         // Required empty public constructor
@@ -51,7 +52,6 @@ public class FindTravelFragment extends Fragment {
         recyclerView = (RecyclerView) view.findViewById(R.id.travel_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         Log.i("gggg", "++ ON START ++");
-        travelList = new ArrayList<>();
         travelList.add(
                 new travel(1, R.drawable.profpic, "Goa(India)", "Naman Kumar", "18", "December End"));
         travelList.add(
@@ -70,6 +70,13 @@ public class FindTravelFragment extends Fragment {
         RadioButton radioButton2 = (RadioButton) view.findViewById(R.id.radioButton2);
         FloatingActionButton fab =(FloatingActionButton) view.findViewById(R.id.travel_addfab);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+
         return view;
     }
 
@@ -80,6 +87,26 @@ public class FindTravelFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+
+    }
+    public void openDialog(){
+        TravelDialog travelDialog = new TravelDialog();
+        travelDialog.setTargetFragment(FindTravelFragment.this, 1);
+        travelDialog.show(getFragmentManager(), "FindTravelFragment");
+    }
+
+    @Override
+    public void sendBackToFragment(String tripName, String tripDetail, String Place, String Date, String City) {
+        rtripName = tripName;
+        rtripDetail = tripDetail;
+        rPlace = Place;
+        rDate = Date;
+        rCity = City;
+        travelList.add(new travel(travelList.size()+1, R.drawable.profpic, rPlace+"("+rCity+")","Naman Kumar", rtripDetail, rDate));
+        recyclerView = (RecyclerView) view.findViewById(R.id.travel_list);
+        adapter = new TravelAdapter(view.getContext(), travelList);
+        Log.i("hhhh", "done");
+        recyclerView.setAdapter(adapter);
 
     }
 
