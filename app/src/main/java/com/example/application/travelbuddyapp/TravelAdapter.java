6b50535ca23dee8f803a29e7d05a7991738c5357
deadmau5 <ashwat16023@iiitd.ac.in>
2018-12-03 22:29:01 +0530

    trip fetched

--- app/src/main/java/com/example/application/travelbuddyapp/TravelAdapter.java
+++ app/src/main/java/com/example/application/travelbuddyapp/TravelAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class TravelAdapter extends RecyclerView.Adapter<TravelAdapter.TravelViewHolder>{
@@ -16,20 +18,19 @@ public class TravelAdapter extends RecyclerView.Adapter<TravelAdapter.TravelView
 
     //we are storing all the products in a list
     private List<travel> travelList;
-    private OnItemClickListner listener;
+    private OnItemClickListner mListner;
 
     //getting the context and product list with constructor
-    public TravelAdapter(Context mCtx, List<travel> travelList, OnItemClickListner listener) {
+    public TravelAdapter(Context mCtx, List<travel> travelList) {
         this.mCtx = mCtx;
         this.travelList = travelList;
-        this.listener = listener;
     }
-
     public interface OnItemClickListner{
         void onItemClick(int position);
     }
-    public void setOnItemClickListner(OnItemClickListner listner){
-        listener = listner;
+
+    public void setOnItemClickListner(StayAdapter.OnItemClickListner listner){
+        mListner = (OnItemClickListner) listner;
     }
 
     @Override
@@ -37,7 +38,7 @@ public class TravelAdapter extends RecyclerView.Adapter<TravelAdapter.TravelView
         //inflating and returning our view holder
         LayoutInflater inflater = LayoutInflater.from(mCtx);
         View view = inflater.inflate(R.layout.travel_list_item, null);
-        return new TravelAdapter.TravelViewHolder(view,listener);
+        return new TravelViewHolder(view,mListner);
     }
 
 
@@ -46,13 +47,15 @@ public class TravelAdapter extends RecyclerView.Adapter<TravelAdapter.TravelView
         //getting the product of the specified position
         travel trvl = travelList.get(position);
 
+
         //binding the data with the viewholder views
-        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(trvl.getImage()));
+       // holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(trvl.getImage()));
 
         holder.textView_place.setText(trvl.getplace());
         holder.textView_host.setText(trvl.gethost());
         holder.textView_detail.setText(String.valueOf(trvl.getdetails()));
         holder.textView_goingno.setText(String.valueOf(trvl.getno_of_going()));
+       // Glide.with(mCtx).load(travel.getImage()).into(holder.imageView);
 
     }
 
@@ -68,7 +71,7 @@ public class TravelAdapter extends RecyclerView.Adapter<TravelAdapter.TravelView
         TextView textView_host, textView_place, textView_detail, textView_goingno;
         ImageView imageView;
 
-        public TravelViewHolder(View itemView, final OnItemClickListner listner) {
+        public TravelViewHolder(View itemView,final OnItemClickListner listner) {
             super(itemView);
 
             textView_place =itemView.findViewById(R.id.travel_place);
